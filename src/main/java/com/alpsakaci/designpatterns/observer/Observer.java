1891d0c040b2b114f225b89553ab89d7210a14bf
alpsakaci <erenalpsakaci@gmail.com>
2020-08-17 17:24:51 +0300

    Observer pattern added.

---
+++ src/main/java/com/alpsakaci/designpatterns/observer/Observer.java
@@ -0,0 +1,9 @@
+package com.alpsakaci.designpatterns.observer;
+
+public abstract class Observer {
+	
+	protected Subject subject;
+	
+	public abstract void update();
+
+}
---
+++ src/main/java/com/alpsakaci/designpatterns/observer/ObserverPattern.java
@@ -0,0 +1,18 @@
+package com.alpsakaci.designpatterns.observer;
+
+public class ObserverPattern {
+
+	public static void main(String[] args) {
+		Subject subject = new Subject();
+		
+		new BinaryObserver(subject);
+		new OctalObserver(subject);
+		new HexaObserver(subject);
+		
+		System.out.println("First state: 21");
+		subject.setState(21);
+		System.out.println("State change: 10");
+		subject.setState(10);
+	}
+
+}
