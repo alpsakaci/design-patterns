24c94e7235cb5607b0a030aca827d2622ea6579e
alpsakaci <erenalpsakaci@gmail.com>
2020-08-20 15:49:50 +0300

    Visitor pattern added.

---
+++ src/main/java/com/alpsakaci/designpatterns/visitor/VisitorPattern.java
@@ -0,0 +1,10 @@
+package com.alpsakaci.designpatterns.visitor;
+
+public class VisitorPattern {
+
+	public static void main(String[] args) {
+		ComputerPart computer = new Computer();
+		computer.accept(new ComputerPartDisplayVisitor());
+	}
+
+}
